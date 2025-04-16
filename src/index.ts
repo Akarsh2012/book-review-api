abcbfd7933a5c97728a562e26fd802065bf2f1a6
Akarsh Singh <akarshs641@gmail.com>
2025-04-17 01:02:43 +0530

    Initial setup with TypeScript, Express, and MongoDB connection

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Book Reviews API is running 📚🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on ->  http://localhost:${PORT}`);
+});
